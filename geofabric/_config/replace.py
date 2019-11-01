1413b17c61fd19d95cbe06cf55a766b8482aa883
Seaton, Shane (L&W, Black Mountain) <shane.seaton@csiro.au>
2019-11-01 18:02:08 +1100

    Adding the config files, nothing sensitive in here anyway

--- geofabric/_config/replace.py
+++
@@ -1,9 +0,0 @@
-# replace this config file with specific config and rename to __init__.py.py
-from os.path import dirname, realpath, join, abspath
-
-APP_DIR = dirname(dirname(realpath(__file__)))
-TEMPLATES_DIR = join(dirname(dirname(abspath(__file__))), 'view', 'templates')
-STATIC_DIR = join(dirname(dirname(abspath(__file__))), 'view', 'static')
-LOGFILE = APP_DIR + '/flask.log'
-DEBUG = True
-
